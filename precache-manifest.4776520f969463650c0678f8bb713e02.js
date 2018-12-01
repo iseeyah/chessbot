3c9ea918753db4af3a817d0ab500c3bf62975f6b
Edd Mann <the@eddmann.com>
2018-12-01 11:50:16 +0000

    Updates

---
+++ precache-manifest.4776520f969463650c0678f8bb713e02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "06c0dabb1694913a5bff",
+    "url": "/chessbot/static/js/runtime~main.06c0dabb.js"
+  },
+  {
+    "revision": "984e77c082b2b37251d3",
+    "url": "/chessbot/static/js/main.984e77c0.chunk.js"
+  },
+  {
+    "revision": "f8a638152ad060d050bb",
+    "url": "/chessbot/static/js/1.f8a63815.chunk.js"
+  },
+  {
+    "revision": "5df55f1da8002a494f2c712d5188ab20",
+    "url": "/chessbot/index.html"
+  }
+];
